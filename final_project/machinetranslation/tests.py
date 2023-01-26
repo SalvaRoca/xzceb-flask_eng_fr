48101a3f2c7d6d6d2d68c86940ee2c1bf2b60ae1
Salva Roca <salva.roca@icloud.com>
2023-01-26 06:54:48 -0500

    Minor changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,14 +1,29 @@
+'''
+Unit testing for translation functions
+'''
+
 import unittest
-from translator import english_to_french, french_to_english
+from translator.py import english_to_french, french_to_english
 
 class TestTranslator(unittest.TestCase):
+    '''
+    This class includes both tests for English to French and vice versa
+    '''
     def test_english_to_french (self):
+        '''
+        Unit testing for English to French function
+        '''
         self.assertIsNone(english_to_french(None))
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertNotEqual(english_to_french('Hello'), 'Au revoir')
 
     def test_french_to_english (self):
+        '''
+        Unit testing for French to English function
+        '''
         self.assertIsNone(french_to_english(None))
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        self.assertNotEqual(french_to_english('Bonjour'), 'Goodbye')
 
 if __name__ == '__main__':
     unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -2,7 +2,6 @@
 Language Translator by Salva Roca
 """
 
-import json
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -13,7 +12,6 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
@@ -22,10 +20,9 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url(url)
 
-
 def english_to_french(english_text):
     """This function translates English to French"""
-    if english_text == None:
+    if english_text is None:
         return None
     french_translation = language_translator.translate(
         text=english_text,
@@ -36,7 +33,7 @@ def english_to_french(english_text):
 
 def french_to_english(french_text):
     """This function translates French to English"""
-    if french_text == None:
+    if french_text is None:
         return None
     english_translation = language_translator.translate(
         text=french_text,
